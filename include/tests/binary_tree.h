fba0aa57332681165dafafeac447422c2bddcd6a
Josselin Dulongcourty <contact@josselinonduty.fr>
2023-12-12 01:11:48 +0100

    feat: Add tests for binary tree type

---
+++ include/tests/binary_tree.h
@@ -0,0 +1,9 @@
+#ifndef __TESTS_BINARY_TREE_H__
+#define __TESTS_BINARY_TREE_H__
+
+void test_binary_tree_create(void);
+void test_binary_tree_add(void);
+void test_binary_tree_add_overflow(void);
+void test_binary_tree_remove(void);
+
+#endif // __TESTS_BINARY_TREE_H__
